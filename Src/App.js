22bcb08e6ade831f2072a1bf407641b8f648fc7b
Mkeshnitp4u <mukeshnitp4u@gmail.com>
2021-05-28 00:46:23 +0530

    Add splash

--- Src/App.js
+++ Src/App.js
@@ -1,23 +1,31 @@
-import React, {useState, useRef} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
   Text,
-  useColorScheme,
-  View,
 } from 'react-native';
 import Lottie from './Component/lottieView'
 const App=()=>{
+  const [splash, setSplash]= useState(true)
+  useEffect(()=>{
+  setTimeout(()=>setSplash(false), 1500)
+
+  }, [])
   return(
-      <SafeAreaView style={{justifyContent:'center', flex:1, alignItems:'center', backgroundColor:'blue'}}>
+    splash ? (<SafeAreaView style={{justifyContent:'center', flex:1, alignItems:'center', backgroundColor:'brown'}}>
         <Lottie 
         style={{width:200, height:200, }}
         sourcePath={require('./Common/Lottie/weather.json')}
         show
       />
-      </SafeAreaView>
+        <Text style={{color:"white", fontSize:30, fontWeight:'bold'}}>
+          WEATHER INFO
+        </Text>
+      </SafeAreaView>) :
+
+  <SafeAreaView style={{justifyContent:'center', flex:1, alignItems:'center'}}>
+    <Text>Home Page</Text>
+  </SafeAreaView>
+
   )
 }
 
